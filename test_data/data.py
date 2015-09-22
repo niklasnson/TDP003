e55f286bde948edde25448bbc5e83edd7c348d34
Jesper Skoglund <jessk378@localhost>
2015-09-22 15:00:10 +0200

    ändrade informationen i data.json så den blev korrekt, innna hade vi fel data.json fil. i data.py skrev jag till information på search functionen så data_test.py kördes utan felmedelanden.

--- test_data/data.py
+++ test_data/data.py
@@ -14,10 +14,12 @@ def load(filename):
         return None
 
 def get_project_count(db):
+    '''return the number of projects in the database'''
     number = len(db)
     return number
 
 def get_project(db, id):
+    '''fetches a specific project from the database, you choose the project with its ID'''
     for i in db:
         if i["project_no"] == id:
             return i
@@ -26,15 +28,17 @@ def get_project(db, id):
 def search(db, sort_by='start_date', 
            sort_order='desc',
            techniques=None, search=None, search_fields=None):
-
+'''search in the database by entering a search word, the field you want to search in. the function also sorts the fetched data by ascending or descending order. It can also check for specific techniques in each project that were fetched with the search function.'''
+   
     search_results = []
     search_results = get_all_data(db, search, search_fields)
-    if techniques != None:
+    if techniques != None and techniques != []:
         search_results = techs_used(search_results, techniques)
     sort_data(search_results, sort_by, sort_order)
     return search_results
 
 def get_techniques(db):
+    '''fetches all the techniques from the database.'''
     temp = []
     for i in db:
         for item in i["techniques_used"]:
@@ -44,6 +48,7 @@ def get_techniques(db):
     return temp
 
 def get_technique_stats(db):
+    '''Check which projects that have certain techniques and then  returns the id and name of those projects.'''
     techs = get_techniques(db)
     key = ''
     temp = {}
@@ -56,15 +61,17 @@ def get_technique_stats(db):
                 temp[item].append({'id':i["project_no"], "name":i["project_name"]})
     
         temp[item] = sorted(temp[item], key=itemgetter('id'))
-        
-    print(temp['csv'])
     return temp
                 
 #---------homemade functions-------
 
 def get_all_data(db, search, search_fields):
+    '''This is a sub function to the search function. This function search the database for the keyword entered.'''
     free_list = []
     i = 0
+    if search == None and search_fields == None:
+        return db
+
     if search_fields == None:
         for i in db:
             for item in i.items():
@@ -80,33 +87,39 @@ def get_all_data(db, search, search_fields):
         for project in db:
             for item in project.items():
                 for j in search_fields:
-                    if item[0] == j and item[1] == search:
-                        free_list.append(project)
-                        
+                    if isinstance(item[1], int):
+                        if item[0] == j and item[1] == search:
+                            free_list.append(project)
+                    elif isinstance(item[i], list):
+                        if item[0] == j and item[1] == search:
+                            free_list.append(project) 
+                    elif isinstance(item[1], str):
+                        if item[0].lower() == j.lower() and item[1].lower() == search.lower():
+                            free_list.append(project)
     return free_list
 
 def sort_data(data, sort_by="project_no", sort_order ='desc'):
+    '''this function sorts the data that the search function gathered.'''
     sort_order = get_sort_order(sort_order)
     data.sort(key=itemgetter(sort_by), reverse=sort_order)
     print(data)
     
-
 def techs_used(search, techs):
+    '''this function checks if the projects that the search function gathered have certain techniques.'''
     temp = []
     for i in search:
-        for tech in techs:
+        for tech in techs: 
             if tech in i['techniques_used']:
                 temp.append(i)
     return temp
 
 def get_sort_order(value):
+    '''this function determine if you are going to sort the information gathered by the search function in ascending or descending order.'''
     if value == 'desc':
         return True
     else:
         return False
     
-    
-
 #------bullshit functions-------
 
 def main():
@@ -114,10 +127,10 @@ def main():
     #get_project_count(db)
     #get_project(db, 2)
     #get_techniques(db)
-    get_technique_stats(db)
-    #search(db, sort_by='project_name', 
-          # sort_order='desc',
-           #techniques=None, search="no no no", search_fields=None)
+    #get_technique_stats(db)
+    search(db, sort_by='start_date', 
+           sort_order='desc',
+           techniques=[], search='okänt', search_fields=["project_no","project_name","course_name"])
 
 
 if __name__ == "__main__":
