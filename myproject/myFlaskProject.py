34494f0522e8a80c0707fb895c8fb4a178eec268
Niklas Nson <niklasnson@me.com>
2015-10-06 12:47:14 +0200

    today's workload

--- myproject/myFlaskProject.py
+++ myproject/myFlaskProject.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask import url_for
+from flask import redirect
+
+import data
+import config
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", db = data.load("data.json"))
 
 @app.route("/projects")
 def projects():
-    return render_template("index.html")
+    return render_template("projects.html", projects= data.load('data.json'))
 
-@app.route("/project/<id>")
+@app.route('/project/<id>')
 def project(id):
-    return render_template("index.html")
+    return render_template('project.html', project= data.get_project(data.load('data.json'), int(id)), config= config.load())
 
 @app.route("/techniques")
 def techniques():
--- myproject/test_flask.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-     return "Flask hälsar dig välkommen till TDP003"
-if __name__ == "__main__":
-     app.run()
