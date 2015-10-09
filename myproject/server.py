f1afb57343c4fbe92eb27ff68e5935a7c7cea3e7
Niklas Nson <niklasnson@me.com>
2015-10-09 11:24:00 +0200

    major mayhem. now solved.

--- myproject/server.py
+++ myproject/server.py
@@ -9,14 +9,18 @@ import config
 
 app = Flask(__name__)
 
+# ---------------------------------------------------------------------------------------------------- #
 
 def data_load(): 
     """ loads the db """
     return data.load('data.json')
 
+# ---------------------------------------------------------------------------------------------------- #
+
 def config_load(): 
     return config.load('config.json')
 
+# ---------------------------------------------------------------------------------------------------- #
 
 @app.route("/")
 def index():
@@ -31,11 +35,15 @@ def index():
             data= last_project
             )
 
+# ---------------------------------------------------------------------------------------------------- #
+
 @app.route("/projects")
 def projects():
     return render_template("projects.html", 
             projects= data_load(), 
             config= config_load())
+
+# ---------------------------------------------------------------------------------------------------- #
 
 @app.route('/project/<id>')
 def project(id):
@@ -44,9 +52,15 @@ def project(id):
                 int(id)), 
             config= config_load())
 
+# ---------------------------------------------------------------------------------------------------- #
+    
 @app.route("/techniques")
 def techniques():
-    return render_template('techniques.html', config= config_load(), data= data.get_technique_stats(data_load()))
+    return render_template('techniques.html', 
+            config= config_load(), 
+            data= data.get_technique_stats(data_load()))
+
+# ---------------------------------------------------------------------------------------------------- #
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
@@ -70,6 +84,14 @@ def search():
         return render_template('search.html', 
                 config= config_load(), 
                 technics= data.get_techniques(data_load())) 
+
+# ---------------------------------------------------------------------------------------------------- #        
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', config= config_load()), 404
+
+# ---------------------------------------------------------------------------------------------------- #
 
 if __name__ == "__main__":
     app.run(debug=True)
